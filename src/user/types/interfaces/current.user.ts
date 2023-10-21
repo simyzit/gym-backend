93eac52bffcc75b72f09ba85a229de4e5e82c920
eduard-ops <epustynnik@gmail.com>
2023-10-21 19:27:29 +0300

    add

---
+++ src/user/types/interfaces/current.user.ts
@@ -0,0 +1,7 @@
+export interface Current {
+  email: string;
+  name: string;
+  surname: string;
+  avatarURL: string;
+  role: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Auth } from 'src/auth/guards/jwt.guard';
+import { CurrentUser } from './decorators/user.decorator';
+import { UserDocument } from './entities/user.entity';
+import { Current } from './types/interfaces/current.user';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Auth()
+  @Get('current')
+  async getCurrentUser(@CurrentUser() user: UserDocument): Promise<Current> {
+    return {
+      email: user.email,
+      name: user.name,
+      surname: user.surname,
+      avatarURL: user.avatarURL,
+      role: user.role,
+    };
+  }
 }
